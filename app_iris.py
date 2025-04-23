702a0617dd0c68465e92e27f5bcabb1ee1500f89
amikoshimrah <sothingshimrah@gmail.com>
2025-04-23 22:39:04 +0530

    Add files via upload

--- app_iris.py
+++ app_iris.py
@@ -18,9 +18,13 @@ sepal_width = st.number_input("Sepal Width (cm)", min_value=0.0, value=3.5)
 petal_length = st.number_input("Petal Length (cm)", min_value=0.0, value=1.4)
 petal_width = st.number_input("Petal Width (cm)", min_value=0.0, value=0.2)
 
+# Label map
+label_map = {0: "Setosa", 1: "Versicolor", 2: "Virginica"}
+
 # Predict button
 if st.button("Predict Species"):
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     prediction = model.predict(input_data)[0]
+    predicted_label = label_map.get(prediction, "Unknown")
 
-    st.success(f"🌼 Predicted Iris Species: **{prediction}**")
+    st.success(f"🌼 Predicted Iris Species: **{predicted_label}**")
